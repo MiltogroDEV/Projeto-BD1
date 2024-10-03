9f47f532647c7b06bac6fce81ea1bb2ebdcbd950
MiltogroDEV <emilton.code@gmail.com>
2024-10-03 14:29:23 -0300

    Logged

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,7 +1,4 @@
 const inputCpf = document.getElementById("inputCPF");
-const loginCPF = "adm.in"
-const loginSenha = "admin"
-
 
 let inputValueTemp = inputCpf.value
 
@@ -13,16 +10,15 @@ inputCpf.addEventListener('keypress', (e) => {
     } else if (inputLength === 11){
         inputCpf.value += "-"
     }
-
 })
 
 const senha = document.getElementById("inputSenha")
 
 function Login(){
     if(inputCpf.value.length < 14 || senha.value.length < 5){
-        if(inputCpf.value == loginCPF && senha.value == loginSenha){
-            // window.location.replace("http://127.0.0.1:5500/Pages/inicio.html")
-            window.location.replace("https://google.com")
+        if(inputCpf.value == "1" && senha.value == "1"){
+            window.location = ("http://127.0.0.1:5500/Pages/inicio.html")
+            alert("Logado")
         } else {
             alert("Credenciais inválidas.")
             inputCpf.value = ""
