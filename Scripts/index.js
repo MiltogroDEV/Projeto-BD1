5219e565e98436d08daad148b0a5e0d4a0521888
MiltogroDEV <emilton.code@gmail.com>
2024-10-16 18:40:16 -0300

    Rooting front to back

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,8 +1,29 @@
 const inputCpf = document.getElementById("inputCPF");
 const senha = document.getElementById("inputSenha");
 
+const inputCpf1 = document.getElementById("inputCPF1");
+const inputNome = document.getElementById("inputNome");
+const senha1 = document.getElementById("inputSenha1");
+const senha2 = document.getElementById("inputSenha2");
+
+let userCpf;
+
 // Função para formatar o CPF enquanto o usuário digita
-inputCpf.addEventListener('keypress', (e) => {
+inputCpf.addEventListener('input', (e) => {
+    let inputLength = inputCpf.value.length;
+    if (inputLength === 3 || inputLength === 7) {
+        inputCpf.value += ".";
+    } else if (inputLength === 11) {
+        inputCpf.value += "-";
+    }
+
+    if (inputLength === 14){
+        let userCpf = inputCpf.value.replace(/[.-]/g, '');
+        console.log(userCpf);
+    }
+});
+
+inputCpf1.addEventListener('input', (e) => {
     let inputLength = inputCpf.value.length;
     if (inputLength === 3 || inputLength === 7) {
         inputCpf.value += ".";
@@ -11,22 +32,57 @@ inputCpf.addEventListener('keypress', (e) => {
     }
 });
 
-const inputCpf1 = document.getElementById("inputCPF1");
-const senha1 = document.getElementById("inputSenha1");
-const senha2 = document.getElementById("inputSenha2");
 
 // Função para cadastrar usuário
-function Cadastrar(){
+function cadastrar(){
     if(inputCpf1.value.length < 14 || senha1.value.length < 5 || senha2.value.length < 5 || senha1.value != senha2.value){
         showMessage("error", "Credenciais inválidas.");
         inputCpf.value =  ""
         senha1.value = ""
         senha2.value = ""
     } else {
+        // endereço do servidor
+        const HOST_API = "http://127.0.0.1:8080"
+        // criar um JSON com os dados que você buscar ou inserir
+        const data = {
+            "name": `${inputNome.value}`,
+            "email": "email",
+            "password": `${senha1.value}`,
+            "cpf": `${userCpf.value}`,
+            "rua": "teste",
+            "numero": "teste",
+            "bairro": "teste",
+            "cidade": "teste",
+            "estado": "teste",
+            "role": "Participant",
+            // "role": "Professor",
+            // "role": "ADM",
+        }
+
+        // para o navegador criar a requisição
+        const requestInfo = {
+            method: "POST", // ou "GET", "POST", "PUT", "DELETE" dependendo do seu caso
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data) // inserir os dados json aqui
+        };
+        
+        // fetch abre a conexao com o banco de dados
+        fetch(`${HOST_API}/create/user`, requestInfo)
+        .then(response => response)
+        .then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.error("Erro na requisição:", error);
+        });
+
         showMessage("success", "Conta criada com sucesso!!!");
-        setTimeout(() => {
-            window.location.href = "http://127.0.0.1:5500/Pages/login.html";
-        }, 2000);
+        // setTimeout(() => {
+        //     // window.location.href = "http://127.0.0.1:5500/Pages/login.html";
+        //     console.log("passou")
+        //     window.location.href = "/login.html";
+        // }, 2000);
     }
 }
 
@@ -53,7 +109,7 @@ function showMessage(type, message) {
 }
 
 // Função de login
-function Login(event) {
+function login(event) {
     event.preventDefault();
     
     if (inputCpf.value.length === 14 && senha.value.length >= 5) {
@@ -80,7 +136,10 @@ function Login(event) {
 }
 
 const loginButton = document.getElementById("loginBtn");
-loginButton.addEventListener("click", Login);
+loginButton.addEventListener("click", login);
+
+const cadastrarButton = document.getElementById("cadastrarBtn");
+loginButton.addEventListener("click", cadastrar);
 
 const logCadBtns = document.getElementById("logCadBtns");
 const avatar = document.getElementById("avatar");
@@ -101,4 +160,3 @@ function Logged() {
 }
 
 loginButton.addEventListener('click', Logged);
-
