8aeb7199de374459de26276931c78ec7fa37cc16
MiltogroDEV <emilton.code@gmail.com>
2024-10-18 13:59:59 -0300

    Melhora FrontEnd

---
+++ Scripts/cadastrar.js
@@ -0,0 +1,147 @@
+
+const inputNome = document.getElementById("inputNome");
+const inputCpf = document.getElementById("inputCPF");
+const inputEmail = document.getElementById("inputEmail");
+const inputTelefone = document.getElementById("inputTelefone");
+const inputCidade = document.getElementById("inputCidade");
+const inputEstado = document.getElementById("inputEstado");
+const inputRua = document.getElementById("inputRua");
+const inputBairro = document.getElementById("inputBairro");
+const inputNumero = document.getElementById("inputNumero");
+const inputSenha1 = document.getElementById("inputSenha1");
+const inputSenha2 = document.getElementById("inputSenha2");
+
+let userCpf;
+let userTelefone;
+
+inputCpf.addEventListener('input', (e) => {
+    let inputLength = inputCpf.value.length;
+    if (inputLength === 3 || inputLength === 7) {
+        inputCpf.value += ".";
+    } else if (inputLength === 11) {
+        inputCpf.value += "-";
+    }
+
+    if (inputLength === 14){
+        let userCpf = inputCpf.value.replace(/[.-]/g, '');
+        console.log(userCpf);
+    }
+});
+
+// Mascara para inputTelefone
+inputTelefone.addEventListener('input', (e) => {
+    let inputLength = inputTelefone.value.length;
+
+    if (inputLength === 1){
+        inputTelefone.value = "(" + inputTelefone.value;
+    } else if (inputLength === 3){
+        inputTelefone.value += ") ";
+    } else if (inputLength === 10){
+        inputTelefone.value += "-";
+    }
+
+    if (inputLength === 15){
+        let userTelefone = inputTelefone.value.replace(/[()\s-]/g, '');
+        console.log(userTelefone);
+    }
+});
+
+// Função para mostrar o popup
+function showMessage(type, message) {
+    const messageBox = document.getElementById("messageBox");
+    
+    if (type === "success") {
+        messageBox.className = "alert alert-success";
+        messageBox.style.backgroundColor = "rgba(60, 255, 0, 0.253)";
+    } else if (type === "error") {
+        messageBox.className = "alert alert-danger";
+        messageBox.style.backgroundColor = "rgba(255, 0, 0, 0.253)";
+    }
+    
+    messageBox.textContent = message;
+    
+    // messageBox.style.display = "block";
+    messageBox.style.display = "inline";
+    
+    setTimeout(() => {
+        messageBox.style.display = "none";
+    }, 3000);
+}
+
+// Função para cadastrar usuário
+function cadastrar(){
+    // if(inputCpf.value.length < 14 || inputSenha1.value.length < 5 || inputSenha2.value.length < 5 || inputSenha1.value != inputSenha2.value){
+    //     showMessage("error", "Credenciais inválidas.");
+    //     inputCpf.value =  ""
+    //     inputSenha1.value = ""
+    //     inputSenha2.value = ""
+    // } else {
+        // endereço do servidor
+        const HOST_API = "https://academic-events-api-83ac51d23457.herokuapp.com"
+        // criar um JSON com os dados que você buscar ou inserir
+        // const data = {
+        //     "cpf": `${userCpf}`,
+        //     "name": `${inputNome.value}`,
+        //     "telefone": `${userTelefone}`,
+        //     "email": `${inputEmail.value}`,
+        //     "password": `${inputSenha1.value}`,
+        //     "rua": `${inputRua.value}`,
+        //     "numero": `${inputNumero.value}`,
+        //     "bairro": `${inputBairro.value}`,
+        //     "cidade": `${inputCidade.value}`,
+        //     "estado": `${inputEstado.value}`,
+        //     "role": "PARTICIPANT",
+        //     // "role": "professor",
+        //     // "role": "administrador",
+        // }
+        const data = {
+            "cpf": "cpf",
+            "name": "nome",
+            "telefone": "telefone",
+            "email": "email",
+            "password": "password",
+            "rua": "rua",
+            "numero": "numero",
+            "bairro": "bairro",
+            "cidade": "cidade",
+            "estado": "estado",
+            "role": "PARTICIPANT",
+            // "role": "professor",
+            // "role": "administrador",
+        }
+
+        // para o navegador criar a requisição
+        const requestInfo = {
+            method: "POST", // ou "GET", "POST", "PUT", "DELETE" dependendo do seu caso
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data) // inserir os dados json aqui
+        };
+
+        console.log(data)
+        
+        // fetch abre a conexao com o banco de dados
+        fetch(`${HOST_API}/create/user`, requestInfo)
+        .then(response => response)
+        .then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.error("Erro na requisição:", error);
+        });
+
+        showMessage("success", "Conta criada com sucesso!!!");
+        // setTimeout(() => {
+        //     console.log("Passou")
+        //     window.location.href = "../Pages/login.html";
+        // }, 3000);
+    }
+// }
+
+const cadastrarButton = document.getElementById("cadastrarBtn");
+cadastrarButton.addEventListener("click", cadastrar);
+
+// TESTES
+// setInterval(() => {
+//     console.log(inputEstado.value)
+// }, 100)
--- Scripts/index.js
+++ Scripts/index.js
@@ -1,12 +1,7 @@
 const inputCpf = document.getElementById("inputCPF");
 const senha = document.getElementById("inputSenha");
 
-const inputCpf1 = document.getElementById("inputCPF1");
-const inputNome = document.getElementById("inputNome");
-const senha1 = document.getElementById("inputSenha1");
-const senha2 = document.getElementById("inputSenha2");
-
-let userCpf;
+// let userCpf;
 
 // Função para formatar o CPF enquanto o usuário digita
 inputCpf.addEventListener('input', (e) => {
@@ -17,74 +12,12 @@ inputCpf.addEventListener('input', (e) => {
         inputCpf.value += "-";
     }
 
-    if (inputLength === 14){
-        let userCpf = inputCpf.value.replace(/[.-]/g, '');
-        console.log(userCpf);
-    }
+    // Tira os .'s e -'s
+    // if (inputLength === 14){
+    //     let userCpf = inputCpf.value.replace(/[.-]/g, '');
+    //     console.log(userCpf);
+    // }
 });
-
-inputCpf1.addEventListener('input', (e) => {
-    let inputLength = inputCpf.value.length;
-    if (inputLength === 3 || inputLength === 7) {
-        inputCpf.value += ".";
-    } else if (inputLength === 11) {
-        inputCpf.value += "-";
-    }
-});
-
-
-// Função para cadastrar usuário
-function cadastrar(){
-    if(inputCpf1.value.length < 14 || senha1.value.length < 5 || senha2.value.length < 5 || senha1.value != senha2.value){
-        showMessage("error", "Credenciais inválidas.");
-        inputCpf.value =  ""
-        senha1.value = ""
-        senha2.value = ""
-    } else {
-        // endereço do servidor
-        const HOST_API = "http://127.0.0.1:8080"
-        // criar um JSON com os dados que você buscar ou inserir
-        const data = {
-            "name": `${inputNome.value}`,
-            "email": "email",
-            "password": `${senha1.value}`,
-            "cpf": `${userCpf.value}`,
-            "rua": "teste",
-            "numero": "teste",
-            "bairro": "teste",
-            "cidade": "teste",
-            "estado": "teste",
-            "role": "Participant",
-            // "role": "Professor",
-            // "role": "ADM",
-        }
-
-        // para o navegador criar a requisição
-        const requestInfo = {
-            method: "POST", // ou "GET", "POST", "PUT", "DELETE" dependendo do seu caso
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data) // inserir os dados json aqui
-        };
-        
-        // fetch abre a conexao com o banco de dados
-        fetch(`${HOST_API}/create/user`, requestInfo)
-        .then(response => response)
-        .then(data => {
-            console.log(data);
-        }).catch(error => {
-            console.error("Erro na requisição:", error);
-        });
-
-        showMessage("success", "Conta criada com sucesso!!!");
-        // setTimeout(() => {
-        //     // window.location.href = "http://127.0.0.1:5500/Pages/login.html";
-        //     console.log("passou")
-        //     window.location.href = "/login.html";
-        // }, 2000);
-    }
-}
 
 // Função para mostrar o popup
 function showMessage(type, message) {
@@ -127,36 +60,50 @@ function login(event) {
     } else if (inputCpf.value == "1" && senha.value == "1") {    
         showMessage("success", "Login bem-sucedido! Redirecionando...");
         setTimeout(() => {
-            window.location.href = "http://127.0.0.1:5500/index.html";
-        }, 2000);
+            console.log("Passou")
+            window.location.href = "/index.html";
+        }, 3000);
         logged = true;
     } else {
         showMessage("error", "Por favor, preencha o CPF e a senha corretamente.");
     }
 }
 
-const loginButton = document.getElementById("loginBtn");
-loginButton.addEventListener("click", login);
+function login(){
+    if(inputCpf.value.length != 14 && senha.value.length < 5){
+        showMessage("error", "Credenciais inválidas.");
+        inputCpf.value =  ""
+        senha.value = ""
+    } else {
+        const HOST_API = "http://127.0.0.1:8080"
+        const data = {
+            "cpf": `${userCpf.value}`,
+            "password": `${senha.value}`,
+        }
 
-const cadastrarButton = document.getElementById("cadastrarBtn");
-loginButton.addEventListener("click", cadastrar);
+        const requestInfo = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
+        
+        fetch(`${HOST_API}/login/user`, requestInfo)
+        .then(response => response)
+        .then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.error("Erro na requisição:", error);
+        });
 
-const logCadBtns = document.getElementById("logCadBtns");
-const avatar = document.getElementById("avatar");
-
-let logged = false;
-
-// Função logado incompleta - fase de testes
-function Logged() {
-    setTimeout(() => {
-        console.log(logged)
-    }, 3000)
-    if (!logged){
-        // logCadBtns.style.display = 'none';
-        // avatar.style.display = 'block';
-        // logged = !logged;
-        console.log("Logado")
+        showMessage("success", "Conta criada com sucesso!!!");
+        setTimeout(() => {
+            console.log("Passou")
+            window.location.href = "../Pages/login.html";
+        }, 3000);
     }
 }
 
-loginButton.addEventListener('click', Logged);
+const loginButton = document.getElementById("loginBtn");
+loginButton.addEventListener("click", login);
